63edbf5b64d53e6d1739320fa260fce98df8abfe
NOTECOLT <6955795+NOTECOLT@users.noreply.github.com>
2025-10-08 14:32:52 +0800

    revert: back to old sprite mechanics

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -1,11 +1,13 @@
 using System;
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting;
 using UnityEngine;
 using UnityEngine.InputSystem;
 
 public class PlayerController : MonoBehaviour
 {
+    public float rotation = 0;
     public float movementSpeed = 7;
     public float turnSpeed = 1;
     public float speedFactorMax = 1.00f;
@@ -13,9 +15,7 @@ public class PlayerController : MonoBehaviour
     public float acceleration = 0.01f; // How fast the car will speed up/down. Expressed as a percentage of the total movementSpeed. 
     PlayerInput _input;
     Rigidbody2D _rb;
-
     float _speedFactor;
-
 
     public event Action OnPlayerCollision;
 
@@ -36,9 +36,13 @@ public class PlayerController : MonoBehaviour
         _input.Player.Disable();
     }
 
+    void Update()
+    {
+    }
+
     void FixedUpdate()
     {
-        float zEuler = transform.localRotation.eulerAngles.z + 90;
+        // float zEuler = transform.localRotation.eulerAngles.z + 90;
 
         if (_input.Player.Brake.IsPressed())
         {
@@ -49,16 +53,22 @@ public class PlayerController : MonoBehaviour
         {
             if (_speedFactor >= -0.5f)
                 _speedFactor -= acceleration;
-        } else
+        }
+        else
         {
             if (_speedFactor <= speedFactorMax)
                 _speedFactor += acceleration;
         }
-    
-        _rb.linearVelocity = new Vector2(Mathf.Cos(zEuler * Mathf.Deg2Rad), Mathf.Sin(zEuler * Mathf.Deg2Rad)).normalized * movementSpeed * 100 * Time.deltaTime;
+
+        _rb.linearVelocity = new Vector2(Mathf.Cos(rotation * Mathf.Deg2Rad), Mathf.Sin(rotation * Mathf.Deg2Rad)).normalized * movementSpeed * 100 * Time.deltaTime;
         _rb.linearVelocity *= _speedFactor;
 
-        transform.Rotate(new Vector3(0, 0, _input.Player.Turn.ReadValue<float>() * turnSpeed * 100 * Time.deltaTime));
+
+        rotation += _input.Player.Turn.ReadValue<float>() * turnSpeed * 100 * Time.deltaTime;
+        if (rotation < 0) rotation += 360;
+        else if (rotation >= 360) rotation -= 360;
+
+        transform.localRotation = Quaternion.Euler(new Vector3(0, 0, rotation + 90));
     }
 
     void OnCollisionEnter2D(Collision2D collision)
@@ -82,3 +92,37 @@ public class PlayerController : MonoBehaviour
         }
     }
 }
+
+
+// if (rotation < 22.5 || rotation > 337.5)
+// {
+//     GetComponent<SpriteRenderer>().sprite = right;
+// }
+// else if (rotation >= 22.5 && rotation <= 67.5)
+// {
+//     GetComponent<SpriteRenderer>().sprite = rightUp;
+// }
+// else if (rotation > 67.5 && rotation < 112.5)
+// {
+//     GetComponent<SpriteRenderer>().sprite = up;
+// }
+// else if (rotation >= 112.5 && rotation <= 157.5)
+// {
+//     GetComponent<SpriteRenderer>().sprite = leftUp;
+// }
+// else if (rotation > 157.5 && rotation < 202.5)
+// {
+//     GetComponent<SpriteRenderer>().sprite = left;
+// }
+// else if (rotation >= 202.5 && rotation <= 247.5)
+// {
+//     GetComponent<SpriteRenderer>().sprite = leftDown;
+// }
+// else if (rotation > 247.5 && rotation < 292.5)
+// {
+//     GetComponent<SpriteRenderer>().sprite = down;
+// }
+// else if (rotation >= 292.5 && rotation <= 337.5)
+// {
+//     GetComponent<SpriteRenderer>().sprite = rightDown;
+// }
